bdca1ae5cbbbc5482ccfccd79106ad82f4889dfb
Timothy Brush <8075964+timothybrush@users.noreply.github.com>
2020-11-24 23:16:49 -0800

    Fixed patch code.

--- debug.sh
+++ debug.sh
@@ -4,8 +4,8 @@ host=$1
 port=$2
 
 /bin/cat <<EOT > debug.patch
---- openssh-7.5p1-mitm.patch	2020-11-25 04:30:59.491498394 +0000
-+++ openssh-7.5p1-mitm.patch-debug	2020-11-25 04:30:43.141951918 +0000
+--- openssh-7.5p1-mitm.patch	2020-11-25 07:10:46.310404602 +0000
++++ openssh-7.5p1-mitm.patch-debug	2020-11-25 07:12:14.545678457 +0000
 @@ -554,7 +554,7 @@
  diff -ru --new-file -x '*~' -x 'config.*' -x Makefile -x opensshd.init -x survey.sh -x openssh.xml -x buildpkg.sh -x output.0 -x requests -x traces.0 -x configure openssh-7.5p1/lol.h openssh-7.5p1-mitm/lol.h
  --- openssh-7.5p1/lol.h	1970-01-01 00:00:00.000000000 +0000
@@ -17,7 +17,7 @@ port=$2
  +
 @@ -564,10 +564,8 @@
  +/* Define these in order to force connections to a test host.
- + * Useful for quickly testing changes without needing to ARP
+ + * Useful for quickly testing changes without needing to ARP 
  + * spoof; just connect to sshd's port directly. */
 -+/*
 -+#define DEBUG_HOST "testhost"
